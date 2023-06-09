29baa3e30c0f604f728635658b1b20922aa7e583
anastasia-maidanyuk <130459788+anastasia-maidanyuk@users.noreply.github.com>
2023-06-09 04:13:34 +0300

    Laboratory work 1
    Loading laboratory work 1

--- main.py
+++ main.py
@@ -1,85 +1,114 @@
 import math
-import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
-import scipy
-from scipy.stats import norm
-from tabulate import tabulate
+import numpy as np
 
-# ЛАБОРАТОРНА РОБОТА No2.
+
+# ЛАБОРАТОРНА РОБОТА No1. ОПИСОВА СТАТИСТИКА
 # Варіант 55
 mu = 5  # математичне сподівання
 sigma = 1.8  # стандартне відхилення
 size = 144  # кількість чисел
-
-# Завдання 1
-def task1():
-    our_array = get_output_array(5, 1.8, 144)
-    print("Завдання 1")
-    print("Масив даних")
-    print(str(our_array))
-    print("Інтервал для 95%:")
-    interval_output(our_array, 0.95, True)
-
-# Завдання 2
-def task2():
-    print("Завдання 2")
-    array_sizes = list(range(125, 5, -5))
-    percentages = [0.99]
-    results_data = []
-
-    for array_size in array_sizes:
-        for percent in percentages:
-            array = get_output_array(5, 1.8, array_size)
-            result = interval_output(array, percent, False)
-            lower_u = round(result.get("down_border_interval"), 5)
-            upper_u = round(result.get("up_border_interval"), 5)
-            lower_sigma = round(result.get("down_sigma"), 5)
-            upper_sigma = round(result.get("up_sigma"), 5)
-            data = {
-                'Розмір': array_size,
-                'Відсоток': str(percent * 100) + '%',
-                'Математичне сподівання': f'{lower_u} < u < {upper_u}',
-                'Середньоквадратичне відхилення': f'{lower_sigma} < σ < {upper_sigma}'
-            }
-            results_data.append(data)
-
-    df = pd.DataFrame(results_data)
-    df = df.set_index('Розмір')
-    print(tabulate(df, headers='keys', tablefmt='pretty'))
+xy = [4, 4, 3, 7, 7, 1, 2, 7, 2, 9, 4, 5, 3, 6, 5, 2, 5, 1, 8, 4, 3, 5, 3, 3, 7, 7, 7, 5, 7, 6, 4, 1, 1, 8, 5, 5, 4, 2,
+      5, 3, 5, 7, 7, 6, 6, 5, 6, 4, 6, 9, 6, 4, 5, 3, 1, 7, 9, 2, 6, 6, 3, 5, 6, 4, 4, 4, 3, 3, 3, 5, 10, 7, 3, 5, 4, 4,
+      4, 9, 3, 5, 2, 5, 3, 6, 4, 6, 2, 3, 5, 9, 8, 3, 6, 3, 5, 4, 6, 6, 5, 2, 6, 4, 7, 5, 5, 5, 6, 5, 9, 6, 2, 4, 7, 3,
+      2, 6, 1, 4, 3, 6, 5, 5, 6, 1, 8, 5, 2, 2, 4, 3, 6, 1, 4, 7, 4, 3, 5, 4, 2, 4, 5, 6, 3, 3]  # генеруємо
 
 
-from scipy.stats import norm, chi2
+# --------------РОЗРАХУНКИ-----------------
+# Вибіркове середнє
+def average(xy):
+    return sum(xy) / len(xy)
 
 
-def interval_output(array, percent, print_values):
-    not_x = np.mean(array)
-    n = len(array)
-    s = np.std(array, ddof=1)
-    t = abs(norm.ppf((1 - percent) / 2, loc=0, scale=1))
-    down_sigma = (n - 1) * (s ** 2) / chi2.ppf(1 - (1 - percent) / 2, n - 1)
-    up_sigma = (n - 1) * (s ** 2) / chi2.ppf((1 - percent) / 2, n - 1)
-    down_border_interval = not_x - (t * s / np.sqrt(n))
-    up_border_interval = not_x + (t * s / np.sqrt(n))
-
-    result = {'down_border_interval': down_border_interval, 'up_border_interval': up_border_interval,
-              'down_sigma': down_sigma, 'up_sigma': up_sigma}
-
-    if print_values:
-        print(f"Вибіркове середнє значення вибірки: {not_x}")
-        print(f"Кількість елементів: {n}")
-        print(f"Середнье відхилення: {s:.3f}")
-        print(f"Критичне значення: {t:.3f}")
-        print(f"Нижня межа інтервалу: {down_border_interval:.3f}")
-        print(f"Верхня межа інтервалу: {up_border_interval:.3f}")
-        print(f"Загальний вигляд: {down_border_interval:.4f} < u < {up_border_interval:.4f}")
-        print(f"Cередньоквадратичне відхилення: {down_sigma:.4f} < σ < {up_sigma:.4f}")
-
-    return result
-
-def get_output_array(mean, variance, size):
-    np.random.seed(0)
-    return np.random.normal(loc=mean, scale=np.sqrt(variance), size=size)
+# Медіану
+def mediana(xy):
+    sorted_xy = sorted(xy)
+    n = len(sorted_xy)
+    if n % 2 == 0:
+        # парна(повертаємо середнє значення двох серединних елементів)
+        return (sorted_xy[n // 2 - 1] + sorted_xy[n // 2]) / 2
+    else:
+        # непарна(повертаємо серединний елемент)
+        return sorted_xy[n // 2]
 
 
-task1()
-task2()
+# Моду
+def moda(xy):
+    count = {}
+    for x in xy:
+        if x in count:
+            count[x] += 1
+        else:
+            count[x] = 1
+    max_count = max(count.values())
+    modas = [num for num, v in count.items() if v == max_count]
+    return modas[0]
+
+
+# Дисперсію
+def calculate_variance(xy):
+    n = len(xy)
+    xy_average = sum(xy)
+    variance = sum((x - xy_average) ** 2 for x in xy) / (n - 1)
+    return variance
+
+
+# Середньоквадратичне відхилення заданої вибірки
+def standard_deviation(xy):
+    n = len(xy)
+    xy_average = sum(xy) / n
+    variance = sum((x - xy_average) ** 2 for x in xy) / (n - 1)
+    std_deviation = math.sqrt(variance)
+    return std_deviation
+
+
+# Виводимо результати
+print(f"Вибіркове середнє: {average(xy)}")
+print(f"Медіана: {mediana(xy)}")
+print(f"Мода: {moda(xy)}")
+print(f"Вибіркова дисперсія: {calculate_variance(xy)}")
+print(f"Середньоквадратичне відхилення: {standard_deviation(xy)}")
+
+
+# --------------ДІАГРАМИ-----------------
+# Графік
+plt.figure(figsize=(18, 12))
+
+
+# Полігон частот
+plt.subplot(231)
+sns.kdeplot(xy, color='pink')
+plt.title('Полігон частот')
+
+
+# Гістограму частот
+plt.subplot(232)
+plt.hist(xy, bins='auto', color='lightblue', alpha=0.83, rwidth=0.76)
+plt.title('Гістограма частот')
+
+
+# Діаграми розмаху
+plt.subplot(234)
+sns.boxplot(xy, color='turquoise')
+plt.title('Діаграма розмаху')
+
+
+# Кругова
+plt.subplot(235)
+bins = pd.cut(xy, bins=10)
+labels, counts = np.unique(bins, return_counts=True)
+plt.pie(counts, labels=labels, colors=sns.color_palette("pastel"))
+plt.title('Кругова діаграма')
+
+
+# Парето
+plt.subplot(236)
+counts = bins.value_counts().sort_values(ascending=False)
+counts.plot(kind='bar', color='orange')
+plt.title('Діаграма Парето')
+
+
+plt.tight_layout()
+plt.show()
